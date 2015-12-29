88de948a41dbf2379c3bd1da9290b53331e46cdc
friend1ws <friend1ws@gmail.com>
2015-12-29 22:27:59 +0900

    modify virus list generation script

--- resource/proc_virus_ref.py
+++ resource/proc_virus_ref.py
@@ -5,17 +5,25 @@ import  gzip
 hout1 = open("viral.genomic.fasta", 'w') 
 hout2 = open("viral_list.txt", 'w')
 
+tempVirus = ""
+tempLength = 0
+tempDescription = ""
 with gzip.open("viral.1.1.genomic.fna.gz", 'r') as hin:
     for line in hin:
         line = line.rstrip('\n')
         if line.startswith('>'):
             F = line.split('|')
             print >> hout1, '>' + F[3]
-            print >> hout2, F[3] + '\t' + F[4].strip(' ')
+            if tempVirus != "":
+                print >> hout2, tempVirus + '\t' + str(tempLength) + '\t' + tempDescription
+            tempVirus = F[3]
+            tempDescription = F[4].strip(' ')
+            tempLength = 0
         else:
+            tempLength = tempLength + len(line)
             print >> hout1, line
-
             
+print >> hout2, tempVirus + '\t' + str(tempLength) + '\t' + tempDescription
 hout1.close()
 hout2.close()
 
